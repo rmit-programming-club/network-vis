33472213961dab7eda89e96e7606c540a2f3f977
Jonathon Belotti <jbelotti@zendesk.com>
2017-04-20 20:53:25 +1000

    render plain project contributors object on about page

--- app/controllers/about_page_controller.rb
+++ app/controllers/about_page_controller.rb
@@ -1,2 +1,10 @@
 class AboutPageController < ApplicationController
+
+  def project_contributors
+    @contributors ||= Octokit.contributors('rmit-programming-club/network-vis')
+  end
+
+  def show
+    render :template => 'about_page/start', :locals => {:data => project_contributors}
+  end
 end
