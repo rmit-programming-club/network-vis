fbb418fd81a8e4812b1bb923ca9b4a5119169e96
Jonathon Belotti <jbelotti@zendesk.com>
2017-04-20 20:53:25 +1000

    render plain project contributors object on about page

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'graph/index'
   get 'graph/data', :defaults => { :format => 'json' }
-  get 'about', to: 'about_page#start'
+
+  get 'about', controller: 'about_page', action: 'show'
 end
