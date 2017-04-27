be1596183e9594730f1e61d1394b4132897f4471
Jonathon Belotti <jbelotti@zendesk.com>
2017-04-27 21:36:41 +1000

    fixing rake tast and all rubocop:changed format problems

--- app/controllers/graph_controller.rb
+++ app/controllers/graph_controller.rb
@@ -1,12 +1,11 @@
 class GraphController < ApplicationController
-  def index
-  end
+  def index; end
 
   def data
     respond_to do |format|
-      format.json {
-        render :json => [1,2,3,4,9]
-      }
+      format.json do
+        render json: [1, 2, 3, 4, 9]
+      end
     end
   end
 end
