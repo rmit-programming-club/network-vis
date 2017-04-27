98dbdec3d30a87114f24f67609703f9a4c9676c4
Louis <louis.stekhoven@gmail.com>
2017-04-27 17:04:06 +1000

    Added a basic example of the octokit api displaying a uers details
    Display
    Users first listed org
    Orgs members
    ORgs first member's repos
    first member's repo commits

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -2,18 +2,48 @@ class OrganizationsController < ApplicationController
 
   ENV['SSL_CERT_FILE'] = File.expand_path(File.dirname(__FILE__)) + "/config/cacert.pem"
 
-  @test
-
+  # TODO add OAth
+  # currently authentication through user, need to change to using
+  # OAuth token and authentication with Organisation rather than a user.
+  # with the org
   def index
+    client = Octokit::Client.new
+
     # Provide authentication credentials
-    Octokit.configure do |c|
+    client.configure do |c|
       c.login = 'LouisKnuckles'
-      c.password = 'PASSWORD'
+      c.password = '#PASSWORD'
     end
 
-    # Fetch the current user
-    user = Octokit.user 'louisknuckles'
-    @test = user.rels[:repos].href
+    # Fetch the current user and their organizations
+    user = client.user
+    organizations = client.organizations(user)
+
+    # TODO add check for a specific org, currently just gets first
+    org = organizations[0][:login]
+    @orgName = org
+    @repos = client.repos(org)
+    @members = client.organization_members(org)
+
+    aMember = @members[0]
+    # Gets the first members first repo
+    @memberRepos = client.repos(aMember[:login])
+    aRepo = @memberRepos[0][:full_name]
+    @memberRepoCommits = client.list_commits(aRepo)
+
+
+
+
+
+#
+#    <thead><b>A members repo commits</b></thead>
+ #   <% @memberRepoCommits.each do |commit| %>
+  #    <tr>
+   # <td><%= commit[:url] %></td>
+    #  </tr>
+ # <% end %>
+#</table>
+
 
   end
 
