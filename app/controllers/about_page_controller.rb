2e063a9644c1c4a24248de4b9d634478827deab0
Louis <louis.stekhoven@gmail.com>
2017-04-27 20:35:38 +1000

    Giving Jono his white space back

--- app/controllers/about_page_controller.rb
+++ app/controllers/about_page_controller.rb
@@ -1,8 +1,9 @@
 class AboutPageController < ApplicationController
 
   def project_contributors
-
+    @contributors ||= Octokit.contributors('rmit-programming-club/network-vis')
   end
+
   def show
     render :template => 'about_page/start', :locals => {:data => project_contributors}
   end
