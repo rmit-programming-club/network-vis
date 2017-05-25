4d0eb991a192dff97891a29c61e34438f36ee560
Jonathon Belotti <jbelotti@zendesk.com>
2017-05-26 09:55:04 +1000

    description string fix

--- spec/controllers/about_page_controller_spec.rb
+++ spec/controllers/about_page_controller_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe AboutPageController, type: :controller do
   describe '#show' do
-    it 'routes to the settings page' do
+    it 'routes to the about page' do
       expect(get: '/about').to \
         route_to(controller: 'about_page',
                  action: 'show')
