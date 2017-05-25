0b5d8545701fc5143ff9f1c11ba13bfa1758be63
Jonathon Belotti <jbelotti@zendesk.com>
2017-05-25 18:26:21 +1000

    getting first test working

---
+++ spec/controllers/about_page_controller_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+describe AboutPageController, type: :controller do
+  describe '#show' do
+    it 'routes to the settings page' do
+      expect(get: '/about').to \
+        route_to(controller: 'about_page',
+                 action: 'show')
+    end
+  end
+end
