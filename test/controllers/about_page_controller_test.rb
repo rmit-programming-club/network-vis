dd2e57c45642aa77fbd833a7db2057333010009b
Jonathon Belotti <jbelotti@zendesk.com>
2017-05-25 19:02:16 +1000

    moving completely from test/ folder to spec/

--- test/controllers/about_page_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test_helper'
-
-class AboutPageControllerTest < ActionDispatch::IntegrationTest
-  test 'should get about page' do
-    get about_url
-    assert_response :success
-  end
-end
