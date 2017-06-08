204447ae18e45352e007d5d018f898037b478fa9
Jonathon Belotti <jbelotti@zendesk.com>
2017-06-08 19:05:14 +1000

    New route for grabbing organisation network data ready for D3

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'organizations/index'
+  get 'organizations/network', defaults: { format: 'json' }
 
   get 'welcome_page/welcome'
   root 'welcome_page#welcome'
