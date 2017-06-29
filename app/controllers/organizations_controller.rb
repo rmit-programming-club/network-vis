68ab6f31ad6b40a5b07e50e37e69e9907823f9d1
rad_rmit <s3529812@student.rmit.edu.au>
2017-06-29 01:05:58 +0000

    Init links array, with 0 values

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -32,18 +32,28 @@ class OrganizationsController < ApplicationController
     # Fetch the current user and their organizations
     user = client.user
     organizations = client.organizations(user)
+    
 
     # TODO add check for a specific org, currently just gets first
     org = organizations[0][:login]
     @orgName = org
     @repos = client.repos(org)
     @members = client.organization_members(org)
+    
+    @member_names = @members.map{|member| member[:login]}
+    @links = Array.new
+    @member_names.combination(2){
+      |m1, m2|
+      @links << {:source => m1, :target => m2, :value => 0}
+    }
+    
 
     aMember = @members[0]
     # Gets the first members
     @memberRepos = client.repos(aMember[:login])
     repo = @memberRepos[0][:full_name]
     @memberRepoCommits = client.list_commits(repo)
+    byebug()
 
   end
 end
