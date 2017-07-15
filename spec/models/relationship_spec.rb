3145956777c03cc5fc5742ed4d788a4b1761fb73
Jonathon Belotti <jbelotti@zendesk.com>
2017-07-15 16:20:08 +1000

    tests (empty) for new models

---
+++ spec/models/relationship_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe Relationship, type: :model doß  
+  def setup
+    @relationship = Relationship.new(follower_id: users(:michael).id,
+                                     followed_id: users(:archer).id)
+  end
+
+  test "should be valid" do
+    assert @relationship.valid?
+  end
+
+  test "should require a follower_id" do
+    @relationship.follower_id = nil
+    assert_not @relationship.valid?
+  end
+
+  test "should require a followed_id" do
+    @relationship.followed_id = nil
+    assert_not @relationship.valid?
+  end
+end
