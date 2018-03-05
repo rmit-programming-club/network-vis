f3634a50aa7e528be61790dce11706f687b7c698
Jonathon Belotti <jonathon.bel.melbourne@gmail.com>
2018-03-05 17:16:16 +1100

    update schema. dont know why it isnt up-to-date

--- db/schema.rb
+++ db/schema.rb
@@ -13,65 +13,59 @@
 ActiveRecord::Schema.define(version: 20170715055838) do
 
   create_table "contributions", force: :cascade do |t|
-    t.integer  "user_id"
-    t.integer  "repository_id"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+    t.integer "user_id"
+    t.integer "repository_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["repository_id"], name: "index_contributions_on_repository_id"
     t.index ["user_id"], name: "index_contributions_on_user_id"
   end
 
   create_table "events", force: :cascade do |t|
-    t.string   "type"
-    t.integer  "repository_id"
-    t.integer  "user_id"
-    t.integer  "organization_id"
-    t.integer  "gh_id"
-    t.boolean  "public"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.string "type"
+    t.integer "repository_id"
+    t.integer "user_id"
+    t.integer "organization_id"
+    t.integer "gh_id"
+    t.boolean "public"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["organization_id"], name: "index_events_on_organization_id"
     t.index ["repository_id"], name: "index_events_on_repository_id"
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
-  create_table "memberships", force: :cascade do |t|
-    t.integer "organization_id"
-    t.integer "user_id"
-    t.index ["organization_id"], name: "index_memberships_on_organization_id"
-    t.index ["user_id"], name: "index_memberships_on_user_id"
-  end
-
   create_table "organizations", force: :cascade do |t|
-    t.string   "url"
+    t.string "url"
+    t.string "public_members"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string   "name"
+    t.string "name"
   end
 
   create_table "relationships", force: :cascade do |t|
-    t.integer  "follower_id"
-    t.integer  "followed_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.integer "follower_id"
+    t.integer "followed_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["followed_id"], name: "index_relationships_on_followed_id"
     t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
     t.index ["follower_id"], name: "index_relationships_on_follower_id"
   end
 
   create_table "repositories", force: :cascade do |t|
-    t.string  "name"
-    t.string  "url"
-    t.string  "owner_type"
+    t.string "name"
+    t.string "url"
+    t.string "owner_type"
     t.integer "owner_id"
-    t.string  "language"
+    t.string "language"
     t.integer "stars"
     t.integer "forks"
     t.index ["owner_type", "owner_id"], name: "index_repositories_on_owner_type_and_owner_id"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "username"
+    t.string "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
